1d69c43c6521ba83979697489104a7f8056d2d9a
Michael Chang <mikeychang2@gmail.com>
2015-02-15 11:59:22 -0800

    minor changes

--- app/js/auth/controller.js
+++ app/js/auth/controller.js
@@ -7,6 +7,8 @@ app.controller('authController', function ($scope, $http, $window) {
   $scope.authenticate = function () {
     $http.post('http://localhost:3000/auth', $scope.user)
       .success(function (data, status, headers, config) {
+        console.log(data)
+        console.log(data.auth_token)
         $window.sessionStorage.token = data.auth_token;
         $scope.message = 'Welcome';
       })
@@ -18,4 +20,11 @@ app.controller('authController', function ($scope, $http, $window) {
         $scope.message = 'Error: Invalid user or password';
       });
   };
+
+   $scope.logout = function () {
+    alert('hi')
+      delete $window.sessionStorage.token;
+      $location.path("/");
+  };
 });
+
