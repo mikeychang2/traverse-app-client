6b1958d2264bafcad418dd450e7b77a2b286a4c2
Karen Lo & Leonard Bui <pair+justkarenlo.lnbcoding@devbootcamp.com>
2015-02-14 21:55:07 -0800

    add delete event logic to eventsController

--- app/js/events/controller.js
+++ app/js/events/controller.js
@@ -20,4 +20,33 @@ app.controller('eventsController', ['$scope', 'tripsFactory', 'eventsFactory', '
 
     $scope.getEvents();
 
+    $scope.insertEvent = function () {
+        var event = $scope.event
+        eventsFactory.insertEvent(event)
+            .success(function (response) {
+                $scope.status = 'Inserted event! Refreshing event list.';
+                $scope.events.push(event);
+                // jquery to append href for new event
+            }).
+            error(function(error) {
+                $scope.status = 'Unable to insert event: ' + error.message;
+            });
+    };
+
+    $scope.deleteEvent = function (id) {
+      eventsFactory.deleteEvent(id)
+          .success(function () {
+              for (var i = 0; i < $scope.events.length; i++) {
+                var checkEvent = $scope.events[i];
+                if (checkEvent.id === id) {
+                  $scope.events.splice(i, 1);
+                  break;
+                }
+              }
+          })
+          .error (function(error) {
+            $scope.status = 'Unable to delete event: ' + error.message;
+          });
+    };
+
 }]);
