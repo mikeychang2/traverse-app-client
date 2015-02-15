f69a494cea67f53b4c56981968362dd686b1bd24
Apprentice <apprentice@DBC-infinity-too-8.local>
2015-02-14 23:53:38 -0800

    delete devise auth things

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('traverseApp', ['ngRoute', 'ng-token-auth']);
+var app = angular.module('traverseApp', ['ngRoute']);
 
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/trips', {
@@ -8,36 +8,3 @@ app.config(['$routeProvider', function ($routeProvider) {
     .otherwise({ redirectTo: '/' })
 }]);
 
-app.config(function($authProvider) {
-  $authProvider.configure({
-            apiUrl: 'http://localhost:3000' //your api's url
-          });
-});
-
-app.controller('authCtrl', function ($scope, $http, $auth) {
-
-  //OAUTH SIGN IN
-  $scope.handleBtnClick = function() {
-    $auth.authenticate('facebook')
-    .then(function(resp) {
-      alert('something successful happened')
-    })
-    .catch(function(resp) {
-        // handle errors
-        // handle errors
-        alert('something terrible happened')
-      });
-  };
-
-  //OAUTH SIGN OUT
-  $scope.handleSignOutBtnClick = function() {
-      $auth.signOut()
-        .then(function(resp) {
-          // handle success response
-        })
-        .catch(function(resp) {
-          // handle error response
-        });
-    };
-
-});
