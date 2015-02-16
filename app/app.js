b09636fc318ff1dd1b7014906627d4672d8e74af
Karen Lo & Leonard Bui <pair+justkarenlo.lnbcoding@devbootcamp.com>
2015-02-15 18:30:17 -0800

    fixed the event routes & added event partial

--- app/app.js
+++ app/app.js
@@ -12,20 +12,28 @@ app.config(['$routeProvider', function ($routeProvider) {
         controller: 'authController',
         templateUrl: 'js/auth/auth.html'
     })
-    .when('/trips/:tripId', {
-        controller: 'eventsController',
-        templateUrl: 'js/events/events.html'
-    })
+    // .when('/trips/:tripId', {
+    //     controller: 'eventsController',
+    //     templateUrl: 'js/events/events.html'
+    // })
     .when('/trips/:tripId/events', {
         controller: 'eventsController',
         templateUrl: 'js/events/events.html'
     })
+    .when('/trips/:tripId/new_event_partial', {
+        controller: 'eventsController',
+        templateUrl: 'js/events/new_event_partial.html'
+    })
+
+
+    .otherwise({ redirectTo: '/' })
+}]);
+
+
+
+// in case we decide to do single event views later:
 
     // .when('/trips/:tripId/events/:eventId', {
     //     controller: 'eventsController',
     //     templateUrl: 'js/events/event.html'
     // })
-
-    .otherwise({ redirectTo: '/' })
-}]);
-
