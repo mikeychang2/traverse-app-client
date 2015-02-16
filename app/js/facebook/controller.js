7706b2b907c248c1ff494a2924aacc34c6710711
Vivian Fialho <vivivan.fialho@gmail.com>
2015-02-16 14:50:07 -0800

    ng-button

--- app/js/facebook/controller.js
+++ app/js/facebook/controller.js
@@ -1,17 +1,29 @@
 app.controller('facebookController', ['$scope', '$http', '$window',
   function ($scope, $http, $window) {
-    $scope.user = "test";
+    $scope.windowStorage = "false";
+
     var urlBase = 'http://localhost:3000';
 
     $scope.initialize = function (){
-      console.log("testingggg!");
       window.fbAsyncInit = function() {
         FB.init({
           appId      : '1411251792503804',
           xfbml      : true,
           version    : 'v2.1'
         });
-      };
+
+        FB.getLoginStatus(function(response) {
+          if (response.status === 'connected') {
+            // $window.sessionStorage.facebook = "true"
+            $scope.windowStorage = "true"
+            $scope.$apply();
+          }
+          else{
+            $window.sessionStorage.facebook = "false"
+            $scope.windowStorage = "false"
+          }
+          })
+        };
 
       (function(d, s, id){
          var js, fjs = d.getElementsByTagName(s)[0];
@@ -24,73 +36,51 @@ app.controller('facebookController', ['$scope', '$http', '$window',
 
     $scope.initialize();
 
-    // $scope.checkUser = function(){
-    //   FB.getLoginStatus(function(response) {
-    //     if (response.status === 'connected') {
-    //       $window.sessionStorage.facebook = true
-    //     }
-    //     else{
-    //       $window.sessionStorage.facebook = false
-    //     }
-    //   }
-    // }
 
     $scope.login = function (){
-      FB.getLoginStatus(function(response) {
-        if (response.status === 'connected') {
-          console.log('Logged in.');
-          console.log(response);
-          var authResponse = response.authResponse
-          $http.get(urlBase + '/facebook' + '?user_id=' + authResponse.userID + "&accessToken=" + authResponse.accessToken)
-            .success(function(response){
-              console.log(response)
-              $window.sessionStorage.facebook = true
-            })
-            .error(function(error){
-              $window.sessionStorage.facebook = false
-              $scope.status = "Access Token expired: " + error.message;
-            });
-        }
-        else {
           FB.login(function(){
            FB.getLoginStatus(function(response){
-            console.log(response);
             var authResponse = response.authResponse
+
             $http.get(urlBase + '/facebook' + '?user_id=' + authResponse.userID + "&accessToken=" + authResponse.accessToken)
             .success(function(response){
               console.log(response)
-              $window.sessionStorage.facebook = true
+              // $window.sessionStorage.facebook = response
+              $scope.windowStorage = response
             })
             .error(function(error){
-              $window.sessionStorage.facebook = false
+              // $window.sessionStorage.facebook = "false"
+              $scope.windowStorage = "false"
               $scope.status = "Unable to save auth response: " + error.message;
             });
+
            })
          })
         }
-      });
-    }
 
 
     $scope.getPhotos = function (){
       $http.get(urlBase + '/facebook/photos')
       .success (function (response) {
-        return response
+        debugger
+        console.log(response)
+        // return response
       })
       .error (function (error) {
         $scope.status = "Unable to retrieve photos: " + error.message;
       });
     }
 
-    $scope.checkAccessToken = function(){
-      $http.get(urlBase + '/facebook/validation')
-      .success (function (response) {
-        $window.sessionStorage.facebook = response
-      })
-      .error (function (error) {
-        $scope.status = "Unable to validate token: " + error.message;
-      });
-    }
+  // check access token expiration and re-validate it (SERVERSIDE: FacebookController: def validation)
+    // $scope.checkAccessToken = function(){
+    //   $http.get(urlBase + '/facebook/validation')
+    //   .success (function (response) {
+    //     $window.sessionStorage.facebook = response
+    //   })
+    //   .error (function (error) {
+    //     $scope.status = "Unable to validate token: " + error.message;
+    //   });
+    // }
 
   }
 ])
--- app/js/facebook/factory.js
+++
@@ -1,5 +0,0 @@
-app.factory('facebookFactory', ['$http', function ($http) {
-
-}])
-
-
