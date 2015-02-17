ae4298a105e435ccde0bdb747f80dd301d473101
justkarenlo <just.karenlo@gmail.com>
2015-02-17 11:35:58 -0800

    working instagram oauth and displaying photos from user :bowtie:

--- app/js/instagram/factory.js
+++ app/js/instagram/factory.js
@@ -4,11 +4,10 @@ app.factory('instagramFactory', ['$http',
     var urlBase = 'http://localhost:3000';
     var instagramFactory = {};
 
-    instagramFactory.igLogin = function () {
-      // window.location.assign("https://api.instagram.com/oauth/authorize/?client_id=cb89d1dac394466e9767ad0e3bf9c6bf&redirect_uri=http://localhost:9000/instagram&response_type=code")
-
-      // return $http.get(urlBase + '/instagram')
+    instagramFactory.igLogin = function (accessCode) {
+      return $http.get(urlBase + '/instagram'  + '?access_token=' + accessCode)
     };
+
     instagramFactory.getPhotos = function () {
       return $http.get(urlBase + '/instagram/photos')
     }
