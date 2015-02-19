df8639b49f2a135fb49dcd4d9c7e9c9cf45824f3
justkarenlo <just.karenlo@gmail.com>
2015-02-18 20:05:21 -0800

    attempted to use default tags

--- app/js/tags/controller.js
+++ app/js/tags/controller.js
@@ -6,6 +6,7 @@ app.controller('tagsController', ['$scope', '$rootScope', 'tagsFactory', 'tripsF
 
     $scope.tags;
     $scope.tag = {};
+    $scope.allDefaultTags;
 
     $scope.tripId = $routeParams.tripId
     $scope.tagId = $routeParams.tagId
@@ -20,6 +21,19 @@ app.controller('tagsController', ['$scope', '$rootScope', 'tagsFactory', 'tripsF
           $scope.status = "Unable to load tags: " + error.message;
         });
     };
+
+    $scope.getDefaultTags = function () {
+        tagsFactory.getDefaultTags()
+        .success(function(response){
+            debugger
+          $scope.allDefaultTags = response
+        })
+        .error(function(error){
+          $scope.status = "Unable to load tags: " + error.message;
+          debugger
+          // console.log(error)
+        });
+    }
 
     $scope.getTagsForEventTagFactory  = function() {
       tagsFactory.getTagsForEventTagFactory ($routeParams.eventId)
